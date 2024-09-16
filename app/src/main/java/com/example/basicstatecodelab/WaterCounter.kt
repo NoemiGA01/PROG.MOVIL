a4ed21b5075a2a5cd6b2fcaedd404761612bfba8
NoemiGA01 <s21120203@alumnos.itsur.edu.mx>
2024-09-15 18:15:37 -0600

    Adjunto Codigo Final

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -4,15 +4,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.material3.Button
 import androidx.compose.foundation.layout.Column
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
-import com.example.basicstatecodelab.ui.theme.WellnessTaskItem
-import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.saveable.rememberSaveable
 
 @Composable
